b73e4fd89ed192dc048b8085d0e63026866c7a59
Mariia Paktovskaia <mpaktovskaya@mail.ru>
2020-11-30 05:15:35 +0300

    Homework 11

---
+++ Lesson-11/Homework-11.js
@@ -0,0 +1,51 @@
+/*Задание 1*/
+var arr = [-1, 0, 2, 34, -2];
+var filterNumbersArr = arr.filter(function(number) {
+  return number > 0;
+} );
+
+alert(filterNumbersArr)
+
+/*Задание 2*/
+var arr = [-1, 0, 2, 34, -2]
+var findItem = arr.find(function(number) {
+  return number > 0;
+});
+
+alert(findItem)
+
+/*Задание 3*/
+function isPalindrome(string){
+    string = string.toLowerCase()
+    var result = '';
+        for(var i = string.length - 1; i >= 0; i--) {
+            result += string[i];
+        }
+    if(string == result){
+        return true}
+    return false
+}
+isPalindrome('довОд')
+
+/*Задание 4*/
+function areAnagrams(word1, word2){
+    word1 = word1.toLowerCase().split('')
+    word2 = word2.toLowerCase().split('')
+    var sorted1 = word1.sort()
+    var sorted2 = word2.sort()
+        if( sorted1.join() == sorted2.join() ) return true;
+    return false
+}
+areAnagrams('довОд', 'вдоод') 
+
+/*Задание 5*/
+function divideArr(arr, size){
+    var tempArr = [];
+
+        for (var i = 0; i < arr.length; i += size) {
+            var formattedArr = arr.slice(i, i+size);
+                tempArr.push(formattedArr);
+        }
+    return tempArr;
+}
+  divideArr([1,2,3,4,5,6,7,8], 3)
