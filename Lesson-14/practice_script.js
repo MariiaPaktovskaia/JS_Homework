5ea30be3e4738b2067297e440affb1e5f8964b3b
Mariia Paktovskaia <mpaktovskaya@mail.ru>
2020-12-12 19:03:22 +0300

    'Homework-14'

---
+++ Lesson-14/practice_script.js
@@ -0,0 +1,45 @@
+localStorage.clear();
+var container = document.getElementById('container');
+
+var firstPar = document.createElement('p');
+var secondPar = document.createElement('p');
+
+firstPar.innerHTML = 'Hello, here are <a href="https://www.facebook.com">Link 1</a> and <a href="https://twitter.com">Link 2</a>';
+secondPar.innerHTML = 'Hello, here are <a href="http://google.by">Link 3</a> and <a href="https://vk.com">Link 4</a>';
+
+container.appendChild(firstPar);
+container.appendChild(secondPar);
+
+var button = document.getElementsByTagName('button');
+var button1 = button[0];
+var searchLinks = firstPar.children;
+
+button1.onclick = changeStyles;
+function changeStyles(EO) {
+    for (var i=0; i<searchLinks.length; i++) {
+        searchLinks[i].classList.toggle('linksStyle');
+    };
+};
+secondPar.addEventListener("click", function(event) {
+    var target = event.target;
+    if (target === secondPar.firstElementChild || target === secondPar.lastElementChild) {
+        event.preventDefault();
+        var storedAge = window.localStorage.getItem(target.text);
+            if ( !storedAge ) {
+        var object ={};
+        object.path = target.getAttribute('href');
+        object=JSON.stringify(object);
+        window.localStorage.setItem(target.text, object);
+        var href = target.setAttribute('href','#');
+        alert('Ссылка сохранена');
+    }
+    else {
+        alert(storedAge); 
+    }
+};
+});
+window.onstorage = function (event){
+    window.localStorage.clear();
+};
+
+
